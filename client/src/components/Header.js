696049c51bd220de6dfa2207df8e3cc3e03d0025
shrki416 <coach-ahmed@bmsnj.org>
2020-04-05 19:48:01 -0400

    Add a header compononet

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Todo List</h1>
+        <p className="lead">A simple todo app</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
